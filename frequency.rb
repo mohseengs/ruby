39705e89c491839886965e2c8b78613284c7a955
mohseengs <125539402+mohseengs@users.noreply.github.com>
2023-02-27 16:16:09 +0530

    Update frequency.rb

--- frequency.rb
+++ frequency.rb
@@ -1,21 +1,5 @@
 str = "mosheen"
-fre = Hash.new
-fre.default = 0
+fre = Hash.new(0)
 
-# str.each_char do |i|
-#     fre[i] += 1
-# end
-
-# str.each_char {|i| fre[i] += 1}
-# puts fre
-
-for ch in str.split("")
-    if fre.key? ch
-        fre[ch] += 1
-    else
-        fre[ch] = 1
-        
-    end
-end
-
+str.each_char {|i| fre[i] += 1}
 puts fre
